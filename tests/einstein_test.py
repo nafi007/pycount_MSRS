bff5bf953ea67cb516aa6530c65069ed76fcf5cf
Md Shahriar Rahim Siddiqui <shahriar.naf07@gmail.com>
2022-01-16 20:24:44 -0800

    test: add unit test for count_words

--- tests/einstein_test.py
+++ tests/einstein_test.py
@@ -1,9 +1,9 @@
 from pycounts_MSRS.pycounts_MSRS import count_words
 from collections import Counter
 
+
 def test_count_words():
-    """Test word counting from a file."""
-    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
+    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1,
                         'the': 1, 'same': 1, 'thing': 1, 
                         'over': 2, 'and': 2, 'expecting': 1,
                         'different': 1, 'results': 1})
