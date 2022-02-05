8b35f273893830d18509a7cdf7b725553736bb0a
nafi007 <39423115+nafi007@users.noreply.github.com>
2022-02-05 00:30:59 -0800

    Comment out everything

--- tests/test_pycounts_MSRS.py
+++ tests/test_pycounts_MSRS.py
@@ -1,12 +1,12 @@
-from pycounts_MSRS.pycounts_MSRS import count_words
-#import pycounts_MSRS
-from collections import Counter
+# from pycounts_MSRS.pycounts_MSRS import count_words
+# #import pycounts_MSRS
+# from collections import Counter
 
-def test_count_words():
-    """Test word counting from a file."""
-    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
-                        'the': 1, 'same': 1, 'thing': 1, 
-                        'over': 2, 'and': 2, 'expecting': 1,
-                        'different': 1, 'results': 1})
-    actual = count_words("tests/einstein.txt")
-    assert actual == expected, "Einstein quote counted incorrectly!"
+# def test_count_words():
+#     """Test word counting from a file."""
+#     expected = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
+#                         'the': 1, 'same': 1, 'thing': 1, 
+#                         'over': 2, 'and': 2, 'expecting': 1,
+#                         'different': 1, 'results': 1})
+#     actual = count_words("tests/einstein.txt")
+#     assert actual == expected, "Einstein quote counted incorrectly!"
